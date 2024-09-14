42bb35207c928b5d018bb66e5d2c2ef519ddc66e
rosecodez <rosebeats09@gmail.com>
2024-09-13 21:48:07 -0400

    Create react router

---
+++ frontend/src/components/home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "../assets/x-logo.jpg"
+
+export default function Home () {
+    return(
+        <>
+            <div id="home-left-panel">
+                <img src={logo} alt={"logo"} className="flex"></img>
+            </div>
+            <div id="home-right-panel">
+                
+            </div>
+        </>
+    )
+}
