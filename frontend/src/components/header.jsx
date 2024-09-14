dc8353826944f081f2a7b88e8e68f2afd0c9af95
rosecodez <rosebeats09@gmail.com>
2024-09-14 15:58:24 -0400

    Sketch links for header elements

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -6,11 +6,18 @@ export default function Header() {
     return(
         <header className="flex items-center justify-between pb-[70px]">
             <div id="header-left-panel" className="flex ">
-                <img src={logo} alt={"logo"} className="w-[40px]"></img>
+                <a href="/">
+                    <img src={logo} alt={"logo"} className="w-[40px]"></img>
+                </a>
+                
             </div>
             <div id="header-left-panel" className="flex gap-6 font-medium">
-                <li className="no-underline hover:underline decoration-2 decoration-sky-500 underline-offset-8">Features</li>
-                <li className="no-underline hover:underline decoration-2 decoration-sky-500 underline-offset-8">Privacy & safety</li>
+                <a href="/features">
+                    <li className="no-underline hover:underline decoration-2 decoration-sky-500 underline-offset-8">Features</li>
+                </a>
+                <a href="privacy">
+                    <li className="no-underline hover:underline decoration-2 decoration-sky-500 underline-offset-8">Privacy & safety</li>
+                </a>
             </div>
         </header>
     )
