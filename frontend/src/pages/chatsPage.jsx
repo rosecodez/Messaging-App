4b34d01dc06d28c0e98283d71572b4e090561aab
rosecodez <rosebeats09@gmail.com>
2024-09-24 20:44:17 -0400

    Overflow textarea container to max 4 rows on event

--- frontend/src/pages/chatsPage.jsx
+++ frontend/src/pages/chatsPage.jsx
@@ -51,9 +51,24 @@ export default function ChatsPage() {
                             /* when selected, hide img button insert,
                             input container box will stretch, become bigger if the user adds more characters */
                         }
-                        <input type="text" name="text" className="size-fit mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
-                        placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="Aa" />
-                        <button className="bg-blue-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Send</button>
+
+                        <textarea
+                            name="text"
+                            onInput={(e) => {
+                                if (e.target.value === '') {
+                                    e.target.style.height = '30px'
+                                } else {
+                                    e.target.style.height = 'auto';
+                                e.target.style.height = `${e.target.scrollHeight}px`;
+                                }
+                            }}
+                            className="max-h-[88px] h-[30px] w-full px-3 py-1 bg-white border shadow-sm border-slate-300 
+                                placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 
+                                block rounded-md sm:text-sm focus:ring-1 overflow-auto resize-none"
+                            placeholder="Aa"
+                        ></textarea>
+
+                        <button>Send</button>
                         
                     </div>
                 </div>
