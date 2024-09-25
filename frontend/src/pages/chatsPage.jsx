df2aef488d329d3950270c2c35d71df4789e97c8
rosecodez <rosebeats09@gmail.com>
2024-09-24 21:05:09 -0400

    Overflow auto for users and messages sections

--- frontend/src/pages/chatsPage.jsx
+++ frontend/src/pages/chatsPage.jsx
@@ -10,24 +10,48 @@ export default function ChatsPage() {
     return (
         <div>
             <ProfileHeader />
-            <div id="chats" className="border border-sky-500 flex">
-                <div id="chats-left-side" className="border border-red-500 w-[300px]">
-                    <div>
+            <div id="chats" className="border border-sky-500 flex max-h-[700px]">
+                <div id="chats-left-side" className="border border-red-500 w-[300px] flex flex-col">
                         <header className="text-2xl font-medium pb-2">Chats</header>
                         <div>
                             <img src={searchIcon} className="pt-2 pl-1 pr-1  w-[30px] absolute" />
                             <input type="search" name="search" className="mb-4 pl-8 mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 
                             placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="Search Messenger" />
                         </div>
-                        <div>Users</div>
-                        <div>Users</div>
-                        <div>Users</div>
-                        <div>Users</div>
-                        <div>Users</div>
-                        <div>Users</div>
-                        <div>Users</div>
-                        <div>Users</div>
-                    </div>
+                        <div className="overflow-auto ">
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                            <div>Users</div>
+                        </div>
                 </div>
 
                 <div id="chats-right-side" className="pl-3 flex flex-col border border-green-500 w-full justify-between">
@@ -39,8 +63,30 @@ export default function ChatsPage() {
                         <img src={threeDots} className="ml-auto pb-2 w-[23px]" />
                     </div>
 
-                    <div id="chats-main">
+                    <div id="chats-main" className="overflow-auto">
                         <div>Text</div>
+                        <div>Text</div>
+                        <div>Text</div>
+                        <div>Text</div>
+                        <div>Text</div>
+                        <div>Text</div>
+                        <div>Text</div>
+                        <div>Text</div><div>Text</div>
+                        <div>Text</div>
+                        <div>Text</div>
+                        <div>Text</div><div>Text</div>
+                        <div>Text</div>
+                        <div>Text</div>
+                        <div>Text</div><div>Text</div>
+                        <div>Text</div>
+                        <div>Text</div>
+                        <div>Text</div><div>Text</div>
+                        <div>Text</div>
+                        <div>Text</div>
+                        <div>Text</div><div>Text</div>
+                        <div>Text</div>
+                        <div>Text</div>
+                        <div>Text</div><div>Text</div>
                         <div>Text</div>
                         <div>Text</div>
                         <div>Text</div>
