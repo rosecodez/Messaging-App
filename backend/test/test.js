c85093d9f27ac2d55c2234efe3bfa81f5a2b7267
rosecodez <rosebeats09@gmail.com>
2024-10-08 19:25:55 -0400

    Test get route get all contacts

--- backend/test/test.js
+++ backend/test/test.js
@@ -1,29 +1,37 @@
 const request = require("supertest");
 const app = require("../app.js");
 const { expect } = require("chai");
-
+require("dotenv").config();
 let agent = request.agent(app);
-describe("GET /users/get-all-contacts", () => {
-  before((done) => {
-    agent
-      .post("/users/log-in")
-      .send({ username: "cristinaqwe", password: "qweasdzxcqweasdzxc" })
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        done();
-      });
-  });
 
-  it("should return a 200 status and a json response with a 'contacts' array of objects", (done) => {
-    agent
-      .get("/users/get-all-contacts")
-      .expect(200)
-      .expect("Content-Type", /json/)
-      .end((err, res) => {
-        if (err) return done(err);
-        expect(res.body).to.be.an("array");
-        done();
-      });
+before((done) => {
+  agent
+    .post("/users/log-in")
+    .send({
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+    })
+    .expect(200)
+    .end((err, res) => {
+      if (err) {
+        return done(err);
+      }
+      done();
+    });
+});
+
+describe("users routes", () => {
+  describe("GET /users/get-all-contacts", () => {
+    it("should return a 200 status and a JSON response with a 'contacts' array of objects", (done) => {
+      agent
+        .get("/users/get-all-contacts")
+        .expect(200)
+        .expect("Content-Type", /json/)
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.body).to.be.an("array");
+          done();
+        });
+    });
   });
 });
